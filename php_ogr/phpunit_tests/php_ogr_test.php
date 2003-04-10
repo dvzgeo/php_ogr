5f3b4a3ad8e3083a3478c100782c1f437afd5bcb
nsavard <>
2003-04-10 04:08:18 +0000

    Adding test suites.

--- php_ogr/phpunit_tests/php_ogr_test.php
+++ php_ogr/phpunit_tests/php_ogr_test.php
@@ -5,6 +5,7 @@ require_once 'php_ogr_testcase1.php';
 require_once 'php_ogr_testcase2.php';
 require_once 'php_ogr_testcase3.php';
 require_once 'php_ogr_testcase4.php';
+require_once 'php_ogr_testcase5.php';
 require_once 'phpunit-0.5/phpunit.php';
 
 /*
@@ -36,18 +37,15 @@ echo $result -> toString();
 
 printf("-----------Beginning suite 3--------- \n");
 
-$suite3 = new PHPUnit_TestSuite("OGRLayerTest1");
+$suite3 = new PHPUnit_TestSuite("OGRLayerTest0");
+$suite3->addTest(new PHPUnit_TestSuite
+                ("OGRLayerTest1"));
 
 $result = PHPUnit::run($suite3);
 
 echo $result -> toString();
 
 ?> 
-
-
-
-
-
 
 
 
