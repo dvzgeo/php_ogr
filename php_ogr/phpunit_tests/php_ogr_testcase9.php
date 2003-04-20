6f6ab68c8a6c6ded474fc67370c482c3991de2ce
nsavard <>
2003-04-20 18:02:44 +0000

    Added testOGR_F_UnsetField1() and clean up.

--- php_ogr/phpunit_tests/php_ogr_testcase9.php
+++ php_ogr/phpunit_tests/php_ogr_testcase9.php
@@ -1,8 +1,9 @@
-require_once `phpunit-0.5/phpunit.php';
+<?php
+//require_once `phpunit-0.5/phpunit.php';
 require_once 'util.php';
 
 class OGRFeatureTest2 extends PHPUnit_TestCase {
-    var $strPathToDumpData;
+    var $strPathToOutputData;
     var $strTmpDumpFile;
     var $strPathToStandardData;
     var $strPathToData;
@@ -10,28 +11,41 @@ class OGRFeatureTest2 extends PHPUnit_TestCase {
     var $hDS;
     var $hLayer;
     var $strDestDataSource;
- 
+    var $hOGRSFDriver;
+    var $strOutputLayer;
+
     function OGRFeatureTest2($name){
         $this->PHPUnit_TestCase($name);	
     }
-
     function setUp() {
+
         $this->strPathToData = "./data/mif";
-        $this->strPathToStandardData = "./data/testcase";
-	$this->strPathToDumpData = "../../ogrtests/testcase/"
+        $this->strPathToStandardData = "./data/testcase/";
+	$this->strPathToOutputData = "../../ogrtests/testcase/";
 	$this->strTmpDumpFile = "DumpFile.tmp";
 	$this->bUpdate = FALSE;
-        $this->strDestDataSource = "NewDataSource";
+        $this->hOGRSFDriver = null;
+        $this->strDestDataSource = "OutputDS";
+        $this->strOutputLayer = "OutputLayer";
+        $this->hDS = OGROpen($this->strPathToData, $this->bUpdate, 
+                             $this->hOGRSFDriver);
 
-        $hDriver = null;
-        $hDS = OGROpen($this->strPathToData, $this->bUpdate, $hDriver);
-
-	$hLayer = OGR_DS_GetLayer($hDS, 3);
+        $iLayer = 3;
+	$this->hLayer = OGR_DS_GetLayer($this->hDS, $iLayer);
 
 
     }
 
     function tearDown() {
+        OGR_DS_Destroy($this->hDS);
+        if (file_exists($this->strPathToOutputData.$this->strDestDataSource)) {
+            system( "rm -R ".$this->strPathToOutputData.$this->strDestDataSource);
+        }
+
+        if (file_exists($this->strPathToOutputData.$this->strTmpDumpFile)) {     
+            system( "rm ".$this->strPathToOutputData.$this->strTmpDumpFile);
+        }
+
         unset($this->strPathToData);
         unset($this->strPathToStandardData);
         unset($this->strPathToDumpData);
@@ -40,42 +54,50 @@ class OGRFeatureTest2 extends PHPUnit_TestCase {
         unset($this->strDestDataSource);
         unset($this->hDS);
         unset($this->hLayer);
+        unset($this->hOGRSFDriver);
+        unset($this->strOutputLayer);
     }
 /***********************************************************************
 *                            testOGR_F_IsFieldSet()
 ************************************************************************/
 
     function testOGR_F_IsFieldSet() {
-        $iFID = 2;
+        $iFID = 7;
         $hF = OGR_L_GetFeature($this->hLayer, $iFID);
         $iField = 1;
         $bFieldSet = OGR_F_IsFieldSet($hF, $iField);
-        $this->AssertTrue($bFieldSet, "");
-}
+
+        $this->AssertTrue($bFieldSet, "Problem with OGR_F_IsFieldSet(): ".
+                           "Field is supposed to be set");
+
+    }
+
 /***********************************************************************
-*                            testOGR_F_UnsetField()                           
+*                            testOGR_F_UnsetField0()                           
 ************************************************************************/
 
-    function testOGR_F_UnsetField() {
-        $iFID = 2;
-        $hf = OGR_L_GetFeature($this->hLayer, $iFID);
+    function testOGR_F_UnsetField0() {
+        $iFID = 7;
+        $hF = OGR_L_GetFeature($this->hLayer, $iFID);
         $iField =1;
-        OGR_F_UnsetField($hf, $iField);
+        OGR_F_UnsetField($hF, $iField);
         $bFieldSet = OGR_F_IsFieldSet($hF, $iField);
-        $this->AssertFalse($bFieldSet, "");
-    
-}
+
+        $this->AssertFalse($bFieldSet, "Problem with OGR_F_UnsetField(): ".
+                                        "Field is not supposed to be set");
+   
+    }
+
 /***********************************************************************
-*                            testOGR_SetFieldRaw()                             
+*                            testOGR_F_UnsetField1()                             
 ************************************************************************/
 
-    function testOGR_SetFieldRaw() {
-        $iFID = 2;
-        $hFSrc = OGR_L_GetFeature($this->hLayer, $iFID);
-        $iField =2;
-        $hField = OGR_F_GetRawFieldRef($hFSrc, $iField);
+    function testOGR_F_GetUnsetField1() {
+        $strStandardFile = "testOGR_F_UnsetField1";
+
         $iDriver = 5;
-        $hDriver = OGR_GetDriver($iDriver);
+        $hDriver = OGRGetDriver($iDriver);
+
         $astrOptions = null;
         $hDestDS = OGR_Dr_CreateDataSource($hDriver, 
                                                $this->strPathToOutputData.
@@ -86,99 +108,127 @@ class OGRFeatureTest2 extends PHPUnit_TestCase {
             printf("Unable to create destination data source\n");
             return FALSE;
         }
-        $iLayer = 0;
-        $hlayerDest = OGR_DS_GeLayer($hDestDS, $iLayer);
+
+        $hDestLayer = OGR_DS_CreateLayer($hDestDS, $this->strOutputLayer, 
+                                     $hSpatialRef,
+                                     $this->eGeometryType,
+                                     null /*Options*/);
+
         $hFieldDefn = OGR_Fld_Create("id", OFTInteger);
-        $eErr = OGR_L_CreateField($hLayerDest, $hFieldDefn, 0 /*bApproxOK*/);
-        $hFeatureDefn = OGR_L_GetLayerDefn($hLayerDest);
-        $hFDest = OGR_F_Create($hFeatureDefn);
-        OGR_F_SetFieldRaw($hFDest, $iField, $hField);
+        $eErr = OGR_L_CreateField($hDestLayer, $hFieldDefn, 0 /*bApproxOK*/);
 
-        print hFDest
-        system diff
-        $this->AssertNull($iRetVal);
-        OGR_DS_Destroy($hDestDS);
-        OGR_DS_Destroy($hDS);
-}
-/***********************************************************************
-*                            test()                                           
-************************************************************************/
+        $hFeatureDefn = OGR_L_GetLayerDefn($hDestLayer);
+        $hF = OGR_F_Create($hFeatureDefn);
+        $iField = 0;
+        $iValue = 25;
+        OGR_F_SetFieldInteger($hF, $iField, $iValue);
 
-    function testOGR_() {
-
-}
-/***********************************************************************
-*                            test()                                           
-************************************************************************/
-
-    function testOGR_() {
-
-}
-/***********************************************************************
-*                            test()                                           
-************************************************************************/
-
-    function testOGR_() {
-
-}
-/***********************************************************************
-*                            test()                                           
-************************************************************************/
-
-    function testOGR_() {
-
-}
-/***********************************************************************
-*                            test()                                           
-************************************************************************/
-
-    function testOGR_() {
-
-}
-/***********************************************************************
-*                            test()                                           
-************************************************************************/
-
-    function testOGR_() {
-
-}
-/***********************************************************************
-*                            test()                                           
-************************************************************************/
-
-    function testOGR_() {
-
-}
-/***********************************************************************
-*                            test()                                           
-************************************************************************/
-
-    function testOGR_() {
-
-}
-/***********************************************************************
-*                            test()                                           
-************************************************************************/
-
-    function testOGR_() {
-
-}
-/***********************************************************************
-*                            test()                                           
-************************************************************************/
-
-    function testOGR_() {
-
-}
-/***********************************************************************
-*                            test()                                           
-************************************************************************/
-
-    function testOGR_() {
-
-}
-
+        $fpOut = fopen($this->strPathToOutputData.
+                       $this->strTmpDumpFile, "w");
 	
+        if ($fpOut == FALSE) {
+            printf("Dump file creation error\n");
+            return FALSE;
+	}
+        OGR_F_DumpReadable($fpOut, $hF);
+
+        OGR_F_UnsetField($hF, $iField);
+        $bFieldSet = OGR_F_IsFieldSet($hF, $iField);
+
+        OGR_F_DumpReadable($fpOut, $hF);
+
+        OGR_F_Destroy($hF);
+
+        fclose($fpOut);
+
+        system("diff --brief ".$this->strPathToOutputData.
+                $this->strTmpDumpFile.
+                " ".$this->strPathToStandardData.$strStandardFile,
+                $iReval);
+
+        $this->assertFalse($iRetval, "Problem with OGR_F_UnsetField() ".
+                             "Files comparison did not matched.\n");
+
+        OGR_DS_Destroy($hDestDS);
+
+    }
+
+/***********************************************************************
+*                            testOGR_F_GetSetFieldRaw()                             
+************************************************************************/
+
+    function testOGR_F_GetSetFieldRaw() {
+        $strStandardFile = "testOGR_F_GetSetFieldRaw";
+
+        $iFID = 2;
+        $hSrcF = OGR_L_GetFeature($this->hLayer, $iFID);
+        $iField =2;
+        $hField = OGR_F_GetRawFieldRef($hSrcF, $iField);
+        $this->AssertNotNull($hField, "Problem with OGR_F_GetRawFieldRef(): ".
+                                        "handle should not be NULL.");
+        $iDriver = 5;
+        $hDriver = OGRGetDriver($iDriver);
+
+        $astrOptions = null;
+        $hDestDS = OGR_Dr_CreateDataSource($hDriver, 
+                                               $this->strPathToOutputData.
+                                               $this->strDestDataSource,
+                                               $astrOptions );
+
+        if ($hDestDS == null) {
+            printf("Unable to create destination data source\n");
+            return FALSE;
+        }
+
+        $hDestLayer = OGR_DS_CreateLayer($hDestDS, $this->strOutputLayer, 
+                                     $hSpatialRef,
+                                     $this->eGeometryType,
+                                     null /*Options*/);
+
+        $iLayer = 0;
+        $hDestLayer = OGR_DS_GetLayer($hDestDS, $iLayer);
+
+        $hFieldDefn = OGR_Fld_Create("LPOLY_", OFTInteger);
+        $eErr = OGR_L_CreateField($hDestLayer, $hFieldDefn, 0 /*bApproxOK*/);
+
+        $hFeatureDefn = OGR_L_GetLayerDefn($hDestLayer);
+        $hDestF = OGR_F_Create($hFeatureDefn);
+        $iField = 0;
+        OGR_F_SetFieldRaw($hDestF, $iField, $hField);
+
+        $fpOut = fopen($this->strPathToOutputData.
+                       $this->strTmpDumpFile, "w");
+	
+        if ($fpOut == FALSE) {
+            printf("Dump file creation error\n");
+            return FALSE;
+	}
+        OGR_F_DumpReadable($fpOut, $hDestF);
+
+        OGR_F_Destroy($hSrcF);
+        OGR_F_Destroy($hDestF);
+
+        fclose($fpOut);
+
+        system("diff --brief ".$this->strPathToOutputData.
+                $this->strTmpDumpFile.
+                " ".$this->strPathToStandardData.$strStandardFile,
+                $iReval);
+
+        $this->assertFalse($iRetval, "Problem with OGR_F_SetFieldRaw() ".
+                            "or OGR_F_GetRawFieldRef(): ".
+                             "Files comparison did not matched.\n");
+
+        OGR_DS_Destroy($hDestDS);
+
+    }
+
+}
+
+?>
+
+
+
 
 
 
