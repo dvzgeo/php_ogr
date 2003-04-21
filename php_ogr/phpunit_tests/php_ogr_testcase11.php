8be70e5d64041d35a16cac494eada2edbe54f166
nsavard <>
2003-04-21 01:56:45 +0000

    Clean up.

--- php_ogr/phpunit_tests/php_ogr_testcase11.php
+++ php_ogr/phpunit_tests/php_ogr_testcase11.php
@@ -1,8 +1,9 @@
-require_once `phpunit-0.5/phpunit.php';
+<?php
+//require_once `phpunit-0.5/phpunit.php';
 require_once 'util.php';
 
 class OGRFeatureTest4 extends PHPUnit_TestCase {
-    var $strPathToDumpData;
+    var $strPathToOutputData;
     var $strTmpDumpFile;
     var $strPathToStandardData;
     var $strPathToData;
@@ -10,20 +11,25 @@ class OGRFeatureTest4 extends PHPUnit_TestCase {
     var $hDS;
     var $hLayer;
     var $strDestDataSource;
- 
+    var $strDirName;
+
     function OGRFeatureTest4($name){
         $this->PHPUnit_TestCase($name);	
     }
 
     function setUp() {
+        $this->strDirName = "testcase/";
         $this->strPathToData = "./data/mif";
-        $this->strPathToStandardData = "./data/testcase";
-	$this->strPathToDumpData = "../../ogrtests/testcase/"
+        $this->strPathToStandardData = "./data/testcase/";
+	$this->strPathToOutputData = "../../ogrtests/".$this->strDirName;
 	$this->strTmpDumpFile = "DumpFile.tmp";
 	$this->bUpdate = FALSE;
-        $this->strDestDataSource = "NewDataSource";
+        $this->strDestDataSource = "OutputDS.tab";
 
-
+        if (file_exists($this->strPathToOutputData)) {
+            system( "rm -R ".$this->strPathToOutputData);
+        }
+        mkdir($this->strPathToOutputData, 0777);
 
     }
 
@@ -38,20 +44,22 @@ class OGRFeatureTest4 extends PHPUnit_TestCase {
         unset($this->hLayer);
     }
 /***********************************************************************
-*                            testOGR_SetGetStyleString()                      
+*                            testOGR_F_SetGetStyleString()                      
 ************************************************************************/
 
-    function testOGR_SetGetStyleString() {
+    function testOGR_F_SetGetStyleString() {
         $hDriver = null;
         $hSrcDS = OGROpen($this->strPathToData, $this->bUpdate, $hDriver);
 
 	$hSrcLayer = OGR_DS_GetLayer($hSrcDS, 3);
 
         $iFID = 2;
-        $hFSrc = OGR_L_GetFeature($this->hLayer, $iFID);
-        $strStyleString = OGR_F_GetStyleString($hFSrc);
+        $hSrcF = OGR_L_GetFeature($hSrcLayer, $iFID);
+
+        $strStyleString = OGR_F_GetStyleString($hSrcF);
+
         $iDriver = 5;
-        $hDriver = OGR_GetDriver($iDriver);
+        $hDriver = OGRGetDriver($iDriver);
         $astrOptions = null;
         $hDestDS = OGR_Dr_CreateDataSource($hDriver, 
                                                $this->strPathToOutputData.
@@ -63,18 +71,26 @@ class OGRFeatureTest4 extends PHPUnit_TestCase {
             return FALSE;
         }
         $iLayer = 0;
-        $hlayerDest = OGR_DS_GeLayer($hDestDS, $iLayer);
+        $hLayerDest = OGR_DS_GetLayer($hDestDS, $iLayer);
+
         $hFieldDefn = OGR_Fld_Create("id", OFTInteger);
         $eErr = OGR_L_CreateField($hLayerDest, $hFieldDefn, 0 /*bApproxOK*/);
+
         $hFeatureDefn = OGR_L_GetLayerDefn($hLayerDest);
         $hDestF = OGR_F_Create($hFeatureDefn);
+
         OGR_F_SetStyleString($hDestF, $strStyleString);
+
         $strStyleStringOut = OGR_F_GetStyleString($hDestF);
 
-        $expected = strcpy($strStyleString);
-        $this->AssertEquals($expected, $strStyleStringOut);
+        $expected = $strStyleString;
+        $this->AssertEquals($expected, $strStyleStringOut, "Problem with ".
+                            "OGR_F_SetStyleString() or OGR_F_GetStyleString().");
+
+        OGR_F_Destroy($hSrcF);
+        OGR_F_Destroy($hDestF);
         OGR_DS_Destroy($hDestDS);
-        OGR_DS_Destroy($hDS);
+        OGR_DS_Destroy($hSrcDS);
     }
 }
-
+?>
