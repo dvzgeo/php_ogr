5c020a9c6420163ad32ea8de1806e95b745ae6e6
nsavard <>
2003-04-25 21:51:12 +0000

    Added headers.

--- php_ogr/phpunit_tests/php_ogr_test.php
+++ php_ogr/phpunit_tests/php_ogr_test.php
@@ -1,5 +1,6 @@
 <?php
-//---- ogrsfdrivertest.php ----    
+require_once 'phpunit-0.5/phpunit.php';
+
 require_once 'php_ogr_testcase1.php';
 require_once 'php_ogr_testcase2.php';
 require_once 'php_ogr_testcase3.php';
@@ -19,34 +20,48 @@ require_once 'php_ogr_testcase16.php';
 require_once 'php_ogr_testcase17.php';
 //require_once 'php_ogr_testcase18.php'; /*Not with MapInfo File.*/
 
-require_once 'phpunit-0.5/phpunit.php';
 
-/*
-printf("-----------Beginning suite 1--------- \n");
-
-$suite1 = new PHPUnit_TestSuite("OGRSFDriverPathSetGoodhOGRSFDriverNotSetTest");
+printf("
+/***********************************************************************
+*                           Beginning suite 1
+*                          
+************************************************************************/
+");
+$suite1 = new PHPUnit_TestSuite("OGRSFDriverRegistrarTest0");
 $suite1->addTest(new PHPUnit_TestSuite
-                ("OGRSFDriverPathSetBadhOGRSFDriverSetToNULLTest"));
+                ("OGRSFDriverRegistrarTest1"));
 $suite1->addTest(new PHPUnit_TestSuite
-                ("OGRSFDriverPathSetOkhOGRSFDriverSetToNULLTest"));
+                ("OGRSFDriverRegistrarTest2"));
 $suite1->addTest(new PHPUnit_TestSuite
-                ("OGRSFDriverMiscellaneousTest1"));
+                ("OGRSFDriverRegistrarTest3"));
+$suite1->addTest(new PHPUnit_TestSuite
+                ("OGRSFDriverTest0"));
 
 $result = PHPUnit::run($suite1);
 
 echo $result -> toString();
 
-printf("-----------Beginning suite 2--------- \n");
 
-$suite2 = new PHPUnit_TestSuite("OGRDataSourceTest1");
+printf("
+/***********************************************************************
+*                           Beginning suite 2
+*                          
+************************************************************************/
+");
+
+$suite2 = new PHPUnit_TestSuite("OGRDataSourceTest0");
 
 $result = PHPUnit::run($suite2);
 
 echo $result -> toString();
 
 
-
-printf("-----------Beginning suite 3--------- \n");
+printf("
+/***********************************************************************
+*                           Beginning suite 3
+*                          
+************************************************************************/
+");
 
 $suite3 = new PHPUnit_TestSuite("OGRLayerTest0");
 $suite3->addTest(new PHPUnit_TestSuite("OGRLayerTest1"));
@@ -55,9 +70,13 @@ $suite3->addTest(new PHPUnit_TestSuite("OGRLayerTest2"));
 $result = PHPUnit::run($suite3);
 
 echo $result -> toString();
-*/
-printf("-----------Beginning suite 4--------- \n");
 
+printf("
+/***********************************************************************
+*                           Beginning suite 4
+*                          
+************************************************************************/
+");
 $suite4 = new PHPUnit_TestSuite("OGRFeatureTest0");
 $suite4->addTest(new PHPUnit_TestSuite("OGRFeatureTest1"));
 $suite4->addTest(new PHPUnit_TestSuite("OGRFeatureTest2"));
@@ -70,8 +89,12 @@ $result = PHPUnit::run($suite4);
 
 echo $result -> toString();
 
-printf("-----------Beginning suite 5--------- \n");
-
+printf("
+/***********************************************************************
+*                           Beginning suite 6
+*                          
+************************************************************************/
+");
 $suite5 = new PHPUnit_TestSuite("OGRGeometryTest0");
 $suite5->addTest(new PHPUnit_TestSuite("OGRGeometryTest1"));
 
@@ -87,10 +110,12 @@ $result = PHPUnit::run($suite6);
 
 echo $result -> toString();
 
-
-
-printf("-----------Beginning suite 7--------- \n");
-
+printf("
+/***********************************************************************
+*                           Beginning suite 7
+*                          
+************************************************************************/
+");
 $suite7 = new PHPUnit_TestSuite("OGRFeatureDefnTest0");
 $suite7->addTest(new PHPUnit_TestSuite("OGRFeatureDefnTest1"));
 
@@ -98,8 +123,12 @@ $result = PHPUnit::run($suite7);
 
 echo $result -> toString();
 
-
-printf("-----------Beginning suite 8--------- \n");
+printf("
+/***********************************************************************
+*                           Beginning suite 8
+*                          
+************************************************************************/
+");
 
 $suite8 = new PHPUnit_TestSuite("OGRGeometryTest2");
 
@@ -108,12 +137,3 @@ $result = PHPUnit::run($suite8);
 echo $result -> toString();
 
 ?> 
-
-
-
-
-
-
-
-
-
