bac2446a438786362ff5d2e3eb6cbe8c7033f907
nsavard <>
2003-04-25 21:50:10 +0000

    Renamed some variables.  Added functionality to delete tmp working directory. Modified messages in Assert commands.  Modified test fixture.  Cleaned up.

--- php_ogr/phpunit_tests/php_ogr_testcase5.php
+++ php_ogr/phpunit_tests/php_ogr_testcase5.php
@@ -1,25 +1,18 @@
 <?php
-require_once 'phpunit-0.5/phpunit.php';
+//require_once 'phpunit-0.5/phpunit.php';
 require_once 'util.php';
 
 class OGRLayerTest1 extends PHPUnit_TestCase {
+    var $strDirName;
+    var $strPathToOutputData;
+    var $strTmpDumpFile;
     var $strPathToData;
     var $strPathToStandardData;
-    var $strPathToOutputData;
-    var $strPathToDumpData;
-    var $strTmpDumpFile;
     var $bUpdate;
     var $hOGRSFDriver;
-    var $strFilename;
-    var $strOutputFilename;
-    var $strCapability;
-    var $strLayerName;
-    var $hSRS;
-    var $eGeometryType;
-    var $strDialect;
-    var $strFormat;
-    var $strDestDataSource;
-
+    var $hLayer;
+    var $hSrcDataSource;
+    var $iSpatialFilter;
 
     // constructor of the test suite
     function OGRLayerTest1($name){
@@ -29,44 +22,52 @@ class OGRLayerTest1 extends PHPUnit_TestCase {
     // this function is defined in PHPUnit_TestCase and overwritten 
     // here
     function setUp() {
+        $this->strDirName = "testcase/";
+	$this->strPathToOutputData = "../../ogrtests/".$this->strDirName;
+        $this->strTmpDumpFile = "DumpFile.tmp";
         $this->strPathToData = "./data/mif";
         $this->strPathToStandardData = "./data/testcase/";
-        $this->strPathToDumpData = "../../ogrtests/testcase/";
-        $this->strFilename = "NewDataSource";
-        $this->strOutputFilename = "";
-        $this->strTmpDumpFile = "DumpFile.tmp";
         $this->bUpdate = FALSE;
         $this->iSpatialFilter[0] = 3350000;  /*xmin*/
         $this->iSpatialFilter[1] = 4210400;  /*ymin*/
         $this->iSpatialFilter[2] = 3580000;  /*xmax*/
         $this->iSpatialFilter[3] = 4280000;  /*ymax*/
-        $this->strCapability = "OLCFastGetExtent";
-        $this->strLayerName = "LayerPoint";
-        $this->hSRS = null;
-        $this->eGeometryType = wkbPoint;
-        $this->strDialect = ""; /*"Generic SQL"*/
-        $this->strFormat = "MapInfo File";
-        $this->strDestDataSource = "Output";
+
+        if (file_exists($this->strPathToOutputData)) {
+            system( "rm -R ".$this->strPathToOutputData);
+        }
+        mkdir($this->strPathToOutputData, 0777);
+
+
+        OGRRegisterAll();
+
+
+        $this->hOGRSFDriver = OGRGetDriver(5);
+
+        $this->hSrcDataSource =  OGR_Dr_Open($this->hOGRSFDriver,
+                                               $this->strPathToData,
+                                               $this->bUpdate);
+
+        $this->hLayer = OGR_DS_GetLayer($this->hSrcDataSource, 4);
+
     }
     // called after the test functions are executed    
     // this function is defined in PHPUnit_TestCase and overwritten 
     // here    
     function tearDown() {
         // delete your instance
+        OGR_DS_Destroy($this->hSrcDataSource);
+
+        unset($this->strDirname);
+        unset($this->strPathToOutputData);
+        unset($this->strTmpDumpFile);
         unset($this->strPathToData);
         unset($this->strPathToStandardData);
-        unset($this->strPathToDumpData);
-        unset($this->strPathToOutputData);
-        unset($this->strFilename);
-        unset($this->strTmpDumpFile);
-        unset($this->strOutputFilename);
         unset($this->bUpdate);
-        unset($this->strCapability);
-        unset($this->strLayerName);
-        unset($this->hSRS);
-        unset($this->eGeometryType);
-        unset($this->strFormat);
-        unset($this->strDestDataSource);
+        unset($this->hOGRSFDriver);
+        unset($this->hLayer);
+        unset($this->iSpatialFilter);
+        unset($this->hSrcDataSource);
     }
 
 /***********************************************************************
@@ -93,46 +94,35 @@ class OGRLayerTest1 extends PHPUnit_TestCase {
             OGR_G_AddPoint($hSpatialFilter,$this->iSpatialFilter[0],
                            $this->iSpatialFilter[1] , 0.0 );
 
-            printf("Xmin = %s Ymin = %s Xmax = %s Ymax = %s\n", 
-                   $this->iSpatialFilter[0], $this->iSpatialFilter[1],
-                   $this->iSpatialFilter[2], $this->iSpatialFilter[3]);
+        OGR_L_SetSpatialFilter($this->hLayer, $hSpatialFilter);
 
-        $hDriver = OGRGetDriver(5);
+        $hSpatialOut = OGR_L_GetSpatialFilter($this->hLayer);
 
-        $hExistingDataSource =  OGR_Dr_Open($hDriver,
-                                               $this->strPathToData,
-                                               $this->bUpdate);
+        $this->assertNotNull($hSpatialOut, "Problem with ".
+                             "OGR_L_SetSpatialFilter():   Spatial filter ".
+                            "is not supposed to be NULL.\n");
 
-        $hLayer = OGR_DS_GetLayer($hExistingDataSource, 4);
-
-        OGR_L_SetSpatialFilter($hLayer, $hSpatialFilter);
-
-        $hSpatialOut = OGR_L_GetSpatialFilter($hLayer);
-
-        $this->assertNotNull($hSpatialOut, "Spatial filter 
-                            is not supposed to be NULL.\n");
-
-        $fpOut = fopen($this->strPathToDumpData.
+        $fpOut = fopen($this->strPathToOutputData.
                        $this->strTmpDumpFile, "w");
 
         if ($fpOut == FALSE) {
             printf("Dump file creation error\n");
             return FALSE;
         }
+
         OGR_G_DumpReadable($fpOut, $hSpatialOut);
 
         fclose($fpOut);
 
-        OGR_DS_Destroy($hExistingDataSource);
-
-        system("diff --brief ".$this->strPathToDumpData.
-                          $this->strTmpDumpFile.
-                          " ".$this->strPathToStandardData.$strStandardFile,
+        system("diff --brief ".$this->strPathToOutputData.
+                               $this->strTmpDumpFile.
+                             " ".$this->strPathToStandardData.
+                             $strStandardFile,
                              $iRetval);
 
-        printf("retval = %d\n", $iRetval);
-
-        $this->assertFalse($iRetval, "Files have changed.\n");
+        $this->assertFalse($iRetval, "Problem with ".
+                  "OGR_L_SetSpatialFilter() or OGR_L_GetSpatialFilter(): ".
+                  "Files comparison did not matched.\n");
     }
 
 /***********************************************************************
@@ -146,42 +136,32 @@ class OGRLayerTest1 extends PHPUnit_TestCase {
 
         $hSpatialFilter = null;
 
-        $hDriver = OGRGetDriver(5);
-
-        $hExistingDataSource =  OGR_Dr_Open($hDriver,
-                                               $this->strPathToData,
-                                               $this->bUpdate);
-
-        $hLayer = OGR_DS_GetLayer($hExistingDataSource, 4);
-
         $strAttributeFilter = "(PERIMETER <= 100000) AND (LAND_FN_ID > 640)";
 
-        $eErr = OGR_L_SetAttributeFilter($hLayer, $strAttributeFilter);
+        $eErr = OGR_L_SetAttributeFilter($this->hLayer, $strAttributeFilter);
 
         $this->assertEquals(OGRERR_NONE, $eErr, "Attribute filter 
                             is not supposed to returned an error.\n");
 
-        $fpOut = fopen($this->strPathToDumpData.
+        $fpOut = fopen($this->strPathToOutputData.
                        $this->strTmpDumpFile, "w");
 
         if ($fpOut == FALSE) {
             printf("Dump file creation error\n");
             return FALSE;
         }
-        OGRDumpSingleLayer($fpOut, $hLayer, TRUE /*bForce*/);
+        OGRDumpSingleLayer($fpOut, $this->hLayer, TRUE /*bForce*/);
 
         fclose($fpOut);
 
-        OGR_DS_Destroy($hExistingDataSource);
-
-        system("diff --brief ".$this->strPathToDumpData.
+        system("diff --brief ".$this->strPathToOutputData.
                           $this->strTmpDumpFile.
                           " ".$this->strPathToStandardData.$strStandardFile,
-                             $iRetval);
+                          $iRetval);
 
-        printf("retval = %d\n", $iRetval);
-
-        $this->assertFalse($iRetval, "Files have changed.\n");
+        $this->assertFalse($iRetval, "Problem with ".
+                  "OGR_L_SetAttributeFilter(): ".
+                  "Files comparison did not matched.\n");
     }
 }
 ?> 
