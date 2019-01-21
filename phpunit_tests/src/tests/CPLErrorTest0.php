86c1521f593471777e6dfcabb9dcacd1ab78fe6e
Edward Nash <e.nash@dvz-mv.de>
2019-01-21 06:49:39 +0100

    Add tests for CPL error functions

---
+++ phpunit_tests/src/tests/CPLErrorTest0.php
@@ -0,0 +1,133 @@
+<?php
+
+/**
+ * Tests for CPL error handling functions
+ *
+ * @author Edward Nash
+ */
+class CPLErrorTest0 extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @var resource
+     */
+    protected $hGeometry;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setUp()
+    {
+        $this->hGeometry = OGR_G_CreateGeometry(wkbPolygon);
+        $hRing = OGR_G_CreateGeometry(wkbLinearRing);
+        OGR_G_AddPoint($hRing, 123.45, 456.78, 0.0);
+        OGR_G_AddPoint($hRing, 12.34, 456.78, 0.0);
+        OGR_G_AddPoint($hRing, 12.34, 45.67, 0.0);
+        OGR_G_AddPoint($hRing, 123.45, 45.67, 0.0);
+        OGR_G_AddPoint($hRing, 123.45, 456.78, 0.0);
+        OGR_G_AddGeometry($this->hGeometry, $hRing);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function tearDown()
+    {
+        $this->hGeometry = null;
+    }
+
+    /**
+     * Test CPLErrorReset
+     */
+    public function testCPLErrorResetGetLastErrorNo()
+    {
+        @OGR_G_RemoveGeometry($this->hGeometry, 0, true);
+        $actual = CPLGetLastErrorNo();
+        $this->assertInternalType(
+            "integer",
+            $actual,
+            "CPLGetLastErrorNo should return an integer when there was an error"
+        );
+        $this->assertNotSame(
+            OGRERR_NONE,
+            $actual,
+            "CPLGetLastErrorNo should not return OGRERR_NONE when there was an error"
+        );
+        CPLErrorReset();
+        $actual = CPLGetLastErrorNo();
+        $this->assertSame(
+            OGRERR_NONE,
+            $actual,
+            "CPLErrorReset had no effect or CPLGetLastErrorNo did not return OGRERR_NONE when there was no error."
+        );
+    }
+
+    /**
+     * Test CPLGetLastErrorType for no error
+     *
+     * @depends testCPLErrorResetGetLastErrorNo
+     */
+    public function testCPLGetLastErrorType0()
+    {
+        CPLErrorReset();
+        $actual = CPLGetLastErrorType();
+        $this->assertSame(
+            0,
+            $actual,
+            "CPLGetLastErrorType should return 0 when there was no error"
+        );
+    }
+
+    /**
+     * Test CPLGetLastErrorType for an error
+     */
+    public function testCPLGetLastErrorType1()
+    {
+        @OGR_G_RemoveGeometry($this->hGeometry, 0, true);
+        $actual = CPLGetLastErrorType();
+        $this->assertInternalType(
+            "integer",
+            $actual,
+            "CPLGetLastErrorType should return an integer when there was an error"
+        );
+        $this->assertSame(
+            3,
+            $actual,
+            "CPLGetLastErrorType should have returned CE_Failure (3)"
+        );
+    }
+
+    /**
+     * Test CPLGetLastErrorMsg for no error
+     *
+     * @depends testCPLErrorResetGetLastErrorNo
+     */
+    public function testCPLGetLastErrorMsg0()
+    {
+        CPLErrorReset();
+        $actual = CPLGetLastErrorMsg();
+        $this->assertSame(
+            "",
+            $actual,
+            "CPLGetLastErrorMsg should return an empty string when there was no error"
+        );
+    }
+
+    /**
+     * Test CPLGetLastErrorMsg for an error
+     */
+    public function testCPLGetLastErrorMsg1()
+    {
+        @OGR_G_RemoveGeometry($this->hGeometry, 0, true);
+        $actual = CPLGetLastErrorMsg();
+        $this->assertInternalType(
+            "string",
+            $actual,
+            "CPLGetLastErrorMsg should return a string when there was an error"
+        );
+        $this->assertSame(
+            "OGR_G_RemoveGeometry() not supported on polygons yet.",
+            $actual,
+            "CPLGetLastErrorMsg should not return an empty string when there was an error"
+        );
+    }
+}
