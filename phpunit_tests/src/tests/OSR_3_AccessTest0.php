21a7fbea0c8e95b5c9a8b529c241dee9406b5d9e
Edward Nash <e.nash@dvz-mv.de>
2021-11-08 16:21:26 +0100

    bugfix: Handle differing test expectations for OSR_GetAxis()
     * GDAL3+ automatically adds axis information

--- phpunit_tests/src/tests/OSR_3_AccessTest0.php
+++ phpunit_tests/src/tests/OSR_3_AccessTest0.php
@@ -400,15 +400,27 @@ class OSR_3_AccessTest0 extends PHPUnit_Framework_TestCase
     public function testOSR_GetAxis1()
     {
         $actual = OSR_GetAxis(static::$srs31468, "PROJCS", 0);
-        $this->assertNull(
-            $actual,
-            "Result of OSR_GetAxis should be NULL when no explicit definition in PROJCS"
-        );
+        if (OGR_VERSION_NUM >= 3000000) {
+            $this->assertSame(
+                ["name" => "Easting", "orientation" => "EAST"],
+                $actual,
+                "Result of OSR_GetAxis should be automatically set when no explicit definition in PROJCS"
+            );
+        } else {
+            $this->assertNull(
+                $actual,
+                "Result of OSR_GetAxis should be NULL when no explicit definition in PROJCS"
+            );
+        }
         $actual = OSR_GetAxis(static::$srs31468, "GEOGCS", 0);
-        $this->assertNull(
-            $actual,
-            "Result of OSR_GetAxis should be NULL when no explicit definition in GEOGCS"
-        );
+        if (OGR_VERSION_NUM >= 3000000) {
+        } else {
+            $this->assertSame(
+                ["name" => "Easting", "orientation" => "EAST"],
+                $actual,
+                "Result of OSR_GetAxis should be NULL when no explicit definition in GEOGCS"
+            );
+        }
     }
 
     /**
